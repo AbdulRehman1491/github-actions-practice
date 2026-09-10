671e9c5df9f5d0f079ffce92d7524ac02a0f7258
Abdul Rehman <abdulrehman72014@gmail.com>
2026-09-10 09:32:37 -0700

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# This workflow
+# This code is from https:// https://github.com/AbdulRehman1491/github-actions-practice/edit/main/app.py
 from flask import Flask, render_template
 app = Flask(__name__)
 
